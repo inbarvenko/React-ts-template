8ba1b283a60ebe36df5b399e65fd560f79216034
ibarvenko <inga_barvenko@mail.ru>
2024-10-03 10:35:54 +0500

    change architecture

---
+++ src/App.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const HeaderWrapper = styled.div`
+    width: 100%;
+
+    border-bottom: #000000 1px solid;
+    margin-bottom: 10px;
+`;
+
+export default HeaderWrapper;
